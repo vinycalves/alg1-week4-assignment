d6c1ca34f9ce768d161326c989bb21520da1bdc0
vinycius <vinycius.dasilva@jala.university>
2024-07-24 08:35:57 -0300

    Last refactoring and optmizing.

--- main.cpp
+++ main.cpp
@@ -4,48 +4,32 @@
 
 using namespace std;
 
-// Function to check if a number is within the valid range (1 to 100000)
-bool is_a_valid_number(int number)
-{
-    return number >= 1 && number <= 100000;
-}
-
 int main()
 {
     int N;
     cin >> N; // Read the input value
 
     // Check if the input number is valid
-    if (!is_a_valid_number(N))
+    if (!(N >= 1 && N <= 100000))
     {
+        // Check if a number is within the valid range (1 to 100000)
         cerr << "This number should be between 1 and 100000" << endl;
         return 1; // Exit if the number is invalid
     }
 
     // Define sets of available coins and notes
-    unordered_set<int> coins = {1, 2, 5};
-    unordered_set<int> notes = {10, 20, 50, 100, 200};
+    const unordered_set<int> coins_and_notes{1, 2, 5, 10, 20, 50, 100, 200};
 
     // Map to store the count of notes and coins needed
     map<string, int> total_back = {{"notes", 0}, {"coins", 0}};
 
-    // Calculate the number of notes needed
-    for (const auto &note : notes)
+    // Calculate the number of notes and coins needed
+    for (const auto& c_n : coins_and_notes)
     {
-        if (N >= note)
+        if (N >= c_n)
         {
-            total_back["notes"] += N / note; // Add the number of notes of this denomination
-            N %= note; // Update N to the remaining amount
-        }
-    }
-
-    // Calculate the number of coins needed
-    for (const auto &coin : coins)
-    {
-        if (N >= coin)
-        {
-            total_back["coins"] += N / coin; // Add the number of coins of this denomination
-            N %= coin; // Update N to the remaining amount
+            total_back[c_n >= 10 ? "notes" : "coins"] += N / c_n; // Add the number of notes and coins of this denomination
+            N %= c_n; // Update N to the remaining amount
         }
     }
 
